efbc80401f3ac3b99c4fd939f87f97034dcb50d0
Obed Okemsinachi <obedokemsinachi@gmail.com>
2025-09-14 23:47:41 +0100

    [fix] fixed vercel routing

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/plain"},
-        "body": "KLT Backend API is running."
-    }
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/health")
+def health():
+    return jsonify({"message": "KLT Backend API is running."})
